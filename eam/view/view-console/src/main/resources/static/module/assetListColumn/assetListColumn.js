2abf0409424f30286dfac1ab1f4f9dc627decbac
lankl <maillank@qq.com>
2022-07-04 12:44:22 +0800

    集成业务流程

--- eam/view/view-console/src/main/resources/static/module/assetListColumn/assetListColumn.js
+++
@@ -1,8 +0,0 @@
-layui.define([ 'layer','admin','form', 'table', 'util',"element"],
-    function (exports) {
-        function getColList( ) {
-            var result="1";
-            return result;
-        }
-        exports('assetListColumn', assetListColumn);
-    });
