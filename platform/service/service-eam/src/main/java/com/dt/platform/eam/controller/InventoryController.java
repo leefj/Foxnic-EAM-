94b5420bbc8d7d97281775d77770c34b127ae83c
lankl <maillank@qq.com>
2022-11-17 08:14:37 +0800

    完善盘点2.4

--- platform/service/service-eam/src/main/java/com/dt/platform/eam/controller/InventoryController.java
+++ platform/service/service-eam/src/main/java/com/dt/platform/eam/controller/InventoryController.java
@@ -218,6 +218,8 @@ public class InventoryController extends SuperController {
     @ApiParamSupport(ignoreDBTreatyProperties = true, ignoreDefaultVoProperties = true)
     public Result save(InventoryVO inventoryVO) {
         Result result = inventoryService.save(inventoryVO, SaveMode.DIRTY_OR_NOT_NULL_FIELDS, false);
+
+
         return result;
     }
 
