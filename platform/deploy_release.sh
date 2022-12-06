0397ce7267e7a9ea41947b0412021f3290cecade
lankl <maillank@qq.com>
2022-12-06 10:24:50 +0800

    优化标签打印

--- platform/deploy_release.sh
+++ platform/deploy_release.sh
@@ -1,43 +1,54 @@
 #!/bin/sh
-#
+
+method="local"
+hostname=""
+if [[ -n "$1" ]];then
+  hostname=$1
+  method="ops"
+else
+  hostname=`hostname`
+fi
+echo "hostname:$hostname"
+
 release_dir="/tmp/app_deploy"
-hostname=`hostname`
 cur_dir=$(cd `dirname $0`; pwd)
 conf_file=$cur_dir/app.conf
 jar_dir=`cat $conf_file|grep ${hostname}.jar_dir|awk -F "=" '{print $2}'`
 bpm_jar_dir=`cat $conf_file|grep ${hostname}.bpm_jar_dir|awk -F "=" '{print $2}'`
 deploy_dir=`cat $conf_file|grep ${hostname}.deploy_dir|awk -F "=" '{print $2}'`
-
-
 if [[ -d $release_dir ]];then
   rm -rf $release_dir
 fi
 mkdir -p $release_dir
-
 echo "jar_dir:$jar_dir"
 echo "deploy_dir:$deploy_dir"
 echo "conf_file:$conf_file"
 echo "release_dir:$release_dir"
-
 cd $release_dir
 cp -r $deploy_dir/* .
 
+version=2.4.0
+# process app.jar
+
 echo "$release_dir/app/app/lib/"
 ls -rtl $jar_dir/lib/
-
-
-version=2.4.0
 cp -r $jar_dir/lib/* $release_dir/app/app/lib/
 cp $jar_dir/wrapper-all-${version}.jar  $release_dir/app/app/app.jar
 
+# process bpm.jar
 #cp -r $jar_dir/lib/* $release_dir/app/bpm/lib/
 ls $bpm_jar_dir/wrapper-camunda-${version}.RELEASE.jar
 ls $release_dir/app/bpm
 cp $bpm_jar_dir/wrapper-camunda-${version}.jar $release_dir/app/bpm/bpm.jar
-
 echo "start to ys"
 tar -zcvf app_release_last.tar.gz ./*
-open .
+if [[ $method == "local" ]];then
+  open .
+fi
+
+
+#upload
+
 exit 0
 
 
