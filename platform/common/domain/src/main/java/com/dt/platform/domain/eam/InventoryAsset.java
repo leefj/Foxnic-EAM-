aa5faef4bb4c3fcd46335fa9c08a297c7ff62958
lankl <maillank@qq.com>
2022-12-10 09:17:12 +0800

    优化标签打印

--- platform/common/domain/src/main/java/com/dt/platform/domain/eam/InventoryAsset.java
+++ platform/common/domain/src/main/java/com/dt/platform/domain/eam/InventoryAsset.java
@@ -24,7 +24,7 @@ import com.github.foxnic.sql.data.ExprRcd;
  * 盘点明细
  * <p>盘点明细 , 数据表 eam_inventory_asset 的PO类型</p>
  * @author 金杰 , maillank@qq.com
- * @since 2022-12-08 17:12:15
+ * @since 2022-12-10 09:16:12
  * @sign 46288E5E591D52F220A8653A7C24D06C
  * 此文件由工具自动生成，请勿修改。若表结构或配置发生变动，请使用工具重新生成。
 */
