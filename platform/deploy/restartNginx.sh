c0e8525b45104367e06e30af86fd19c82d9d5cc6
Lank <maillank@qq.com>
2023-04-25 11:00:10 +0800

    2.6.0版本

--- platform/deploy/restartNginx.sh
+++ platform/deploy/restartNginx.sh
@@ -2,5 +2,6 @@
 cur_dir=$(cd `dirname $0`; pwd)
 cd $cur_dir
 sh stopNginx.sh
+sleep 1
 sh startNginx.sh
 exit 0
--- platform/deploy/startNginx.sh
+++ platform/deploy/startNginx.sh
@@ -3,6 +3,7 @@ cur_dir=$(cd `dirname $0`; pwd)
 cd $cur_dir
 sh stopNginx.sh
 if [[ -f ./app/nginx/sbin/nginx ]];then
+  echo "to start nginx"
   ./app/nginx/sbin/nginx
 fi
 exit 0
