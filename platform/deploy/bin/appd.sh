c805baad84b55649bf7da1073fd5c66fac4cc4ec
Lank <maillank@qq.com>
2023-05-13 12:23:26 +0800

    2.6.0版本

--- platform/deploy/bin/appd.sh
+++ platform/deploy/bin/appd.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
+#nohup sh appd.sh &
 cur_dir=$(cd `dirname $0`; pwd)
 app_dir=$cur_dir/..
 logFile=$app_dir/logs/appd.log
 cd $cur_dir
+name=app
 while true
 do
   d=`date`
   echo "$d">>$logFile
-  appStatus=`sh app.sh status app|grep "is running"|wc -l`
-  if [[ $appStatus -gt 0 ]];then
-    echo "app status is running" >>$logFile
+  cd $cur_dir
+  statusCnt=`sh app.sh status $name|grep "is running"|grep pid|wc -l`
+  if [[ $statusCnt -gt 0 ]];then
+    echo "$name status is running" >>$logFile
   else
-    echo "app restart" >>$logFile
-    cd $cur_dir/..
-    sh restartApp.sh
+    echo "$name go to restart" >>$logFile
+    cd $cur_dir
+    sh app.sh restart $name
   fi
 sleep 300
 done
