b80086adafa2aed01b8dceb5e77584a1f79d9d36
Lank <maillank@qq.com>
2024-04-24 22:08:54 +0800

    库存完善

--- platform/deploy/bin/sql/c_dh_clear.sql
+++ platform/deploy/bin/sql/c_dh_clear.sql
@@ -1,3 +1,51 @@
+delete from sys_menu where id='833105281570308096';
+insert into sys_menu (`id`, `batch_id`, `authority`, `hidden`, `css`, `label`, `type`, `path_resource_id`, `url`, `params`, `parent_id`, `dynamic_handler`, `hierarchy`, `sort`, `create_by`, `create_time`, `update_by`, `update_time`, `deleted`, `delete_by`, `delete_time`, `version`, `notes`, `link_type`, `open_type`, `extra_url`) VALUES ('833105281570308096', '', 'e5-97ae275b17-8b21', '0', '', '库存统计', 'page', '712023405431357440', '#!stock_stat_data', 'page=action&code=833101722493648896&path=/ureport/preview?_u=db:763449077118533632.ureport.xml', '568725500025896960', '', 'eam/568725500025896960/833105281570308096', '6', '110588348101165911', '2024-04-17 22:22:57', '110588348101165911', '2024-04-17 22:27:18', '0', NULL, NULL, '7', '', 'internal', 'internal_tab', '');
+delete from sys_menu where id='835488379670888448';
+insert into sys_menu(`id`, `batch_id`, `authority`, `hidden`, `css`, `label`, `type`, `path_resource_id`, `url`, `params`, `parent_id`, `dynamic_handler`, `hierarchy`, `sort`, `create_by`, `create_time`, `update_by`, `update_time`, `deleted`, `delete_by`, `delete_time`, `version`, `notes`, `link_type`, `open_type`, `extra_url`) VALUES ('835488379670888448', '', '58-8155be6569-a685', '0', '', '资产库存统计', 'page', '712023405431357440', '#!asset_stock_list_2', 'page=action&code=833341319161053184&path=/ureport/preview?_u=db:763449077118533632.ureport.xml', '471985490283724800', '', 'eam/471985490283724800/835488379670888448', '999999', '613058313164161024', '2024-04-24 12:12:32', '613058313164161024', '2024-04-24 12:16:17', '0', NULL, NULL, '7', '', 'internal', 'internal_tab', '');
+-- 工作台
+delete from sys_menu where hierarchy like '%809752346140606464%';
+delete from sys_menu where hierarchy like '%724920223844532224%';
+delete from sys_menu where hierarchy like '%766183428553965568%';
+delete from sys_menu where hierarchy like '%812060024565137409%';
+delete from sys_menu where hierarchy like '%812055899739783168%';
+delete from sys_menu where hierarchy like '%812059257083002880%';
+delete from sys_menu where hierarchy like '%638295219103596544%';
+delete from sys_menu where hierarchy like '%592349052897595392%';
+delete from sys_menu where hierarchy like '%592349239971942400%';
+delete from sys_menu where hierarchy like '%592349186557480960%';
+delete from sys_menu where hierarchy like '%782907749779374081%';
+delete from sys_menu where hierarchy like '%592348947209523200%';
+delete from sys_menu where hierarchy like '%815210493005266944%';
+-- 设备
+delete from sys_menu where hierarchy like '%823490970048790528%';
+delete from sys_menu where hierarchy like '%829049441838170112%';
+delete from sys_menu where hierarchy like '%823249448208433152%';
+delete from sys_menu where hierarchy like '%829049718699982849%';
+delete from sys_menu where hierarchy like '%823249608607006721%';
+delete from sys_menu where hierarchy like '%829049594716356609%';
+-- 人事
+delete from sys_menu where hierarchy like '%803949358914469888%';
+delete from sys_menu where hierarchy like '%807235387075330049%';
+delete from sys_menu where hierarchy like '%811268060357853184%';
+delete from sys_menu where hierarchy like '%806953516751912960%';
+delete from sys_menu where hierarchy like '%812066149570183169%';
+-- table center
+delete from sys_menu where hierarchy like '%807234885918916609%';
+delete from sys_menu where hierarchy like '%812066338984951809%';
+delete from sys_menu where hierarchy like '%741946151036518401%';
+delete from sys_menu where hierarchy like '%806953516751912960%';
+-- 资产
+delete from sys_menu where hierarchy like '%480488576866320384%';
+delete from sys_menu where hierarchy like '%480492959574261760%';
+delete from sys_menu where hierarchy like '%576882774674251776%';
+delete from sys_menu where hierarchy like '%644115713031667712%';
+delete from sys_menu where hierarchy like '%480481725646897152%';
+delete from sys_menu where hierarchy like '%728254160960487424%';
+delete from sys_menu where hierarchy like '%817777628751593472%';
+delete from sys_menu where hierarchy like '%724683072708018176%';
+delete from sys_menu where hierarchy like '%651093916031909888%';
+delete from sys_menu where hierarchy like '%494597318092259328%';
+delete from sys_menu where hierarchy like '%534772131834101760%';
 -- wms
 delete from sys_menu where hierarchy like '%682489463259004928%';
 -- 保养
