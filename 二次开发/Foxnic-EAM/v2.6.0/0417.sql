c7f3a832118cfcb4eb42e78d3418f71648b30bb2
Lank <maillank@qq.com>
2024-06-22 14:46:45 +0800

    移除wms代码

--- 二次开发/Foxnic-EAM/v2.6.0/0417.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE eam_asset ADD COLUMN `longitude` decimal(10,2) NULL COMMENT '' AFTER `bill_id`;
-ALTER TABLE eam_asset ADD COLUMN `dimension` decimal(10,2) NULL COMMENT '' AFTER `bill_id`;
-
